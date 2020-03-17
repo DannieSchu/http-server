055013d83cf77c7bc3afb600ff4526665746cd57
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-16 21:34:55 -0700

    write and test responses to GET requests for red, green and blue

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,5 +11,56 @@ describe('app routes', () => {
         );
       });
   });
+  // it('creates echo json', () => {
+  //   return request(app)
+  //     .post('/echo')
+  //     .then(res => {
+  //       expect(res.body).toEqual(
+  //         `<html>
+  //         <body>
+  //           <h1>HELLO Hello hello</h1>
+  //         </body>
+  //       </html>`
+  //       );
+  //     });
+  // });
+  it('retrieves red', () => {
+    return request(app)
+      .get('/red')
+      .then(res => {
+        expect(res.text).toEqual(`
+        <html>
+          <body>
+            <h1>Red</h1>
+          </body>
+        </html>`
+        );
+      });
+  });
+  it('retrieves green', () => {
+    return request(app)
+      .get('/green')
+      .then(res => {
+        expect(res.text).toEqual(`
+        <html>
+          <body>
+            <h1>Green</h1>
+          </body>
+        </html>`
+        );
+      });
+  });
+  it('retrieves blue', () => {
+    return request(app)
+      .get('/blue')
+      .then(res => {
+        expect(res.text).toEqual(`
+        <html>
+          <body>
+            <h1>Blue</h1>
+          </body>
+        </html>`
+        );
+      });
+  });
 });
-
